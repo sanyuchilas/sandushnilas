715a940542c570c395b32dffddac30fa7db4d184
sanyuchilas <sanyuchilas@gmail.com>
2024-07-08 22:17:44 +0300

    initialized language and build home page

---
+++ sandushnilas-frontend/i11n.js
@@ -0,0 +1,20 @@
+export default {
+  en: {
+    'page-title-you-can': 'You can',
+    'identity-link-long-text': 'Identity.',
+    'projectivity-link-long-text': 'Projectivity.',
+    'contactivity-link-long-text': 'Contactivity.',
+    'identity-link-short-text': 'I.',
+    'projectivity-link-short-text': 'P.',
+    'contactivity-link-short-text': 'C.',
+  },
+  ru: {
+    'page-title-you-can': 'Ты можешь',
+    'identity-link-long-text': 'Идентичность.',
+    'projectivity-link-long-text': 'Проективность.',
+    'contactivity-link-long-text': 'Контактивность.',
+    'identity-link-short-text': 'И.',
+    'projectivity-link-short-text': 'П.',
+    'contactivity-link-short-text': 'К.',
+  },
+};
---
+++ sandushnilas-frontend/index.js
@@ -0,0 +1,61 @@
+import i11n from './i11n.js';
+
+let currentLanguage = 'en';
+let clientWidth = document.body.clientWidth;
+
+const indentityLinkElement = document.getElementById('identity-link');
+const projectivityLinkElement = document.getElementById('projectivity-link');
+const contactivityLinkElement = document.getElementById('contactivity-link');
+
+const languageSwitcherElement = document.getElementById('language-switcher');
+
+window.addEventListener('resize', onWindowResize);
+languageSwitcherElement.addEventListener('click', onLanguageSwitcherClick);
+
+function renderText() {
+  if (clientWidth < 1024) {
+    renderShortText();
+  } else {
+    renderLongText();
+  }
+
+  languageSwitcherElement.textContent =
+    i11n[currentLanguage]['page-title-you-can'];
+
+  document.title = languageSwitcherElement.textContent + ' ' + '💕';
+}
+
+function renderShortText() {
+  indentityLinkElement.textContent =
+    i11n[currentLanguage]['identity-link-short-text'];
+  projectivityLinkElement.textContent =
+    i11n[currentLanguage]['projectivity-link-short-text'];
+  contactivityLinkElement.textContent =
+    i11n[currentLanguage]['contactivity-link-short-text'];
+}
+
+function renderLongText() {
+  indentityLinkElement.textContent =
+    i11n[currentLanguage]['identity-link-long-text'];
+  projectivityLinkElement.textContent =
+    i11n[currentLanguage]['projectivity-link-long-text'];
+  contactivityLinkElement.textContent =
+    i11n[currentLanguage]['contactivity-link-long-text'];
+}
+
+function onWindowResize() {
+  clientWidth = document.body.clientWidth;
+  renderText();
+}
+
+function onLanguageSwitcherClick() {
+  if (currentLanguage === 'en') {
+    currentLanguage = 'ru';
+  } else {
+    currentLanguage = 'en';
+  }
+
+  renderText();
+}
+
+onWindowResize();
